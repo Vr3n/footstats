a8ce55bce5746e6fa82b717529343e713209e815
Vr3n <vickyspatel@gmail.com>
2024-06-14 18:35:52 +0530

    Integrated Sentry.

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from fastapi import Body, FastAPI
+import sentry_sdk
+from typing import Annotated
+from fastapi import FastAPI, Form
 from fastapi.responses import JSONResponse
 
+from settings import settings
 from database import db_lifespan
 from tasks import create_task
+
+
+sentry_sdk.init(
+    dsn=settings.sentry_dsn,
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
 
 app: FastAPI = FastAPI(lifespan=db_lifespan)
 
@@ -11,15 +21,15 @@ app: FastAPI = FastAPI(lifespan=db_lifespan)
 async def home():
     return {
         "message": "Hello, Friend!",
-        "database": app.database.name,
     }
 
 
 @app.post("/task")
-def run_task(data=Body(...)):
-    amount = int(data['amount'])
-    x = data['x']
-    y = data['y']
+def run_task(amount: Annotated[int, Form()], x: Annotated[int, Form()],
+             y: Annotated[int, Form()]):
+    amount = int(amount)
+    x = int(x)
+    y = int(y)
     task = create_task.delay(amount, x, y)
 
     return JSONResponse({"task": task.get()})
